92854a51c6f8fef9a8176c87eb7aaf5551cb80bd
guilherme <gui.dani13@gmail.com>
2022-04-24 15:24:46 -0300

    added mocktail

--- lib/repositories/post_repository.dart
+++ lib/repositories/post_repository.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import '../models/post_model.dart';
 
 class PostRepository {
-  // final String baseUrl = 'https://jsonplaceholder.typicode.com';
-  List<Post> posts = [];
+  final Client _httpClient;
+
+  PostRepository(this._httpClient);
 
   Future<List<Post>> getPosts() async {
     try {
-      var url = await Uri.https('jsonplaceholder.typicode.com', '/posts',
+      var url = Uri.https('jsonplaceholder.typicode.com', '/posts',
           {'_limit': '20', '_page': '1'});
-      final response = await http.get(url);
+      final response = await _httpClient.get(url);
       final newItems = jsonDecode(response.body);
       return (newItems as List)
           .map<Post>(
@@ -24,21 +24,8 @@ class PostRepository {
             ),
           )
           .toList();
-      // posts.addAll(
-      //   newItems.map<Post>(
-      //     (post) => Post(
-      //       body: post['body'],
-      //       id: post['id'],
-      //       title: post['title'],
-      //       userId: post['userId'],
-      //     ),
-      //   ),
-      // );
-
-      return posts;
     } catch (error) {
       throw Exception(error);
     }
-    return [];
   }
 }
--- test/repositories/post_repository_test.dart
+++ test/repositories/post_repository_test.dart
@@ -1,12 +1,16 @@
 import 'package:fetch_users_01/repositories/post_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
 
 void main() {
-  final postRepo = PostRepository();
+  final postRepo = PostRepository(Client());
 
   test('should get a list os Posts', () async {
     var response = await postRepo.getPosts();
 
     expect(response, isNotNull);
+    expect(response.length, 20);
+    expect(response[0].id, 1);
   });
 }
